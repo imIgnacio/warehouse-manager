6f94dbdef8968fa195780af109e2ef6db674c008
Ignacio Diaz <nachodiaz99@gmail.com>
2021-12-02 20:53:56 +1100

    profile endpoint removed

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,28 +24,6 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/user/:id", async (req, res) => {
-  try {
-    const userData = await User.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
-
-    const currentUser = userData.get({ plain: true });
-
-    res.render("homepage", {
-      ...currentUser,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 // Render inventory endpoint
 router.get("/inventory", async (req, res) => {
   try {
