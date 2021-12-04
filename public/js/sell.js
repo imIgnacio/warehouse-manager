4b0969c7ab5de438a8093b5070142a5ed790049b
Sri <srilalitha.ganti@gmail.com>
2021-12-04 12:15:57 +1100

    selling vehicle by id

--- public/js/sell.js
+++ public/js/sell.js
@@ -1,14 +1,23 @@
 const sellFormHandler = async (event) => {
   event.preventDefault();
-  const id = document.getElementById("search_id").value.trim();
-  console.log(id);
-  // const model = document.getElementById("#vehicle-model").value.trim();
-  // const cost_price = document.getElementById("#costprice").value.trim();
-  // const sell_price = document.getElementById("#sellingprice").value.trim();
-  if (id) {
-    const response = await fetch(`/api/vehicles/sell/${id}`, {
+  const rego_number = document.getElementById("search_id").value.trim();
+  const model = document.getElementById("#vehicle-model").value.trim();
+  const cost_price = document.getElementById("#costprice").value.trim();
+  const sell_price = document.getElementById("#sellingprice").value.trim();
+  if (rego_number) {
+    const response = await fetch(`/api/vehicles/${rego_number}`, {
+      method: "GET",
+
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+  if (rego_number && model && cost_price && sell_price) {
+    const response = await fetch(`/api/vehicles/sell/${rego_number}`, {
       method: "DELETE",
-      body: JSON.stringify({ id }),
+      body: JSON.stringify({ rego_number, model, cost_price, sell_price }),
       headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
@@ -18,4 +27,5 @@ const sellFormHandler = async (event) => {
     }
   }
 };
+
 document.getElementById("sellform").addEventListener("click", sellFormHandler);
